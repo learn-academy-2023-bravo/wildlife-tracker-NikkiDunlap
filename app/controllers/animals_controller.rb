5afca0d8e4c34e5af4f6867bbdf1885e7b3ad556
LEARN Academy <sdunlap102326@gmail.com>
2023-04-21 09:42:05 -0700

    First Story Complete

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -18,8 +18,27 @@ class AnimalsController < ApplicationController
         end
     end
 
-    private 
+    def update
+        animal = Animal.find(params[:id])
+        animal.update(animal_params)
+        if animal.valid?
+            render json: animal
+        else 
+            render json: animals.errors
+        end
+    end
+
+    def destroy
+        animal = Animal.find(params[:id])
+        if animal.destroy
+            render json: animal
+        else
+            render json: animals.errors
+        end
+    end
+    private
     def animal_params
         params.require(:animal).permit(:name, :binomial)
     end
 end
+
